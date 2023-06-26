5072936ecaa71bf236a30dd48e141a9025305928
sylvia <sylviafmast@gmail.com>
2023-06-26 15:30:56 -0400

    Initial Commit

---
+++ AboutMe/AboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeApp.swift
+//  AboutMe
+//
+//  Created by Scholar on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AboutMe/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  AboutMe
+//
+//  Created by Scholar on 6/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        ZStack{
+            Image("Image 2")
+                .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                .frame(width: 900.0, height: 900.0)
+            
+            VStack {
+                Text("Hello, I am Sylvia Mast")
+                
+                Image("Image")
+                    .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                    .frame(width: 300.0, height: 300.0)
+                Button("Click here"){
+                    
+                }
+                .buttonStyle(
+                    .borderedProminent)
+                .tint(.pink)
+                .padding()
+            }
+            .frame(width: 100.0, height: 100.0)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
